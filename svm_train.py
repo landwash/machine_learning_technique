c53dd9474044b6ba7f2165e2a87851a7642a640c
landwash <nirvanazhli@gmail.com>
2015-01-06 00:18:56 +0800

    train using libsvm in python

---
+++ svm_train.py
@@ -0,0 +1,8 @@
+from svmutil import *
+trainfile = open('F:\GitHubRepository\machine_learning_technique\\features.train.txt');
+lines = trainfile.readlines()
+dlist = [line.strip().split() for line in lines]
+
+
+
+
